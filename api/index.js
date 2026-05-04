80fd65f4b9c45611fca0f07c14675bd14ab2b72f
Esmequiinn <esqueralda847@gmail.com>
2026-05-03 19:05:34 -0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -16,21 +16,18 @@ builder.defineStreamHandler(async ({ type, id }) => {
     const [imdbId, s, e] = id.split(':');
     const streams = [];
 
-    // Opción Latino
     streams.push({
         name: "Embed.su",
         title: "ESPAÑOL LATINO / MULTI 🇲🇽",
         url: type === 'movie' ? `https://embed.su/embed/movie/${imdbId}` : `https://embed.su/embed/tv/${imdbId}/${s}/${e}`
     });
 
-    // Opción Inglés New
     streams.push({
         name: "Vidsrc.to",
         title: "ENGLISH / MULTI 🇺🇸 (New)",
         url: type === 'movie' ? `https://vidsrc.to/embed/movie/${imdbId}` : `https://vidsrc.to/embed/tv/${imdbId}/${s}/${e}`
     });
 
-    // Opción Inglés Old
     streams.push({
         name: "Vidsrc.me",
         title: "ENGLISH / MULTI 🌎 (Old)",
@@ -44,7 +41,11 @@ const addonInterface = builder.getInterface();
 const router = getRouter(addonInterface);
 
 module.exports = (req, res) => {
-    router(req, res, () => {
-        res.status(404).send('Not Found');
+    router(req, res, (err) => {
+        if (err) {
+            res.status(500).send({ error: err.message });
+        } else {
+            res.status(404).send('Not Found');
+        }
     });
 };
